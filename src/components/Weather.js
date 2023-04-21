a7a86c6166bde5d6e4fa61191a52736995b4e5c0
Rod512 <rudro149187@bsdi-bd.org>
2023-04-21 13:29:14 +0600

    project done

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -16,22 +16,85 @@ const APIkey = 'fc62695859357c2753c81031c6bc2ca5'
 const Weather = () => {
     const [data, setData] = useState(null)
     const [location, setLocation] = useState('Savar')
+    const [inputValue, setInputValue] = useState('')
+    const [animate, setAnimate] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [errorMsg, setErrorMsg] = useState('')
+
+    const handleInput = (e)=>{
+        setInputValue(e.target.value)
+    }
+
+    const handleSubmit = (e) =>{
+        // if input values is not empty
+        if (inputValue !== ''){
+            // set location
+            setLocation(inputValue)
+        }
+        
+
+        
+        //  select input
+        const input = document.querySelector('input')
+
+        // if input value is empty
+        if (input.value === ''){
+            // set animate to true
+            setAnimate(true)
+
+            setTimeout(()=>{
+                setAnimate(false)
+            },500)
+
+        }
+
+        // clear input
+        input.value = ''
+
+        e.preventDefault()
+    }
+    
+
+
+    
 
     // fetch the data
     useEffect(()=>{
+        setLoading(true)
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${APIkey}`
 
         axios.get(url).then(res => {
-            setData(res.data)
+            // set the data after 1500ms
+
+            setTimeout(()=>{
+                setData(res.data)
+                // set loading to false
+                setLoading(false)
+            }, 1500)
+           
+        }).catch(err=>{
+            setLoading(false)
+            setErrorMsg(err)
         })
     }, [location])
+
+    // err massage
+    useEffect(()=>{
+        const timer = setTimeout(() => {
+            setErrorMsg('')
+            
+        }, 2000);
+        // clear timer
+        return ()=>clearTimeout(timer)
+
+    }, [errorMsg])
 
     //if data is false show the loader
     if(!data){
         return(
-            <div>
+            <div className='w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col justify-center items-center'>
                 <div>
-                    <ImSpinner8 className='text-5xl animate-spin'></ImSpinner8>
+                    <ImSpinner8 className='text-5xl animate-spin text-white'></ImSpinner8>
                 </div>
             </div>
         )
@@ -40,7 +103,7 @@ const Weather = () => {
 
     // set the icon according to the weather
     let icon;
-    console.log(data.weather[0].main)
+    
     switch(data.weather[0].main){
         case 'Clouds':
             icon = <IoMdCloudy/>
@@ -51,19 +114,19 @@ const Weather = () => {
             break;
 
         case 'Rain':
-            icon = <IoMdRainy/>
+            icon = <IoMdRainy className='text-[#31cafb]'/>
             break;
 
         case 'Clear':
-            icon = <IoMdSunny/>
+            icon = <IoMdSunny className='text-[#ffde33]'/>
             break;
 
         case 'Drizzle':
-            icon = <BsCloudDrizzleFill/>
+            icon = <BsCloudDrizzleFill className='text-[#31cafb]'/>
             break;
 
         case 'Snow':
-            icon = <IoMdSnow/>
+            icon = <IoMdSnow className='text-[#31cafb]'/>
             break;
 
         case 'Thunderstrom':
@@ -81,18 +144,24 @@ const Weather = () => {
     return (
         <Fragment>
             <div className='w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col items-center justify-center px-4 lg:px-0'>
+            {errorMsg && <div className='w-full max-w-[90vw] lg:max-w-[450px] bg-[#ff208c] text-white absolute top-2 lg:top-2 p-4 capitalize rounded-md'>{`${errorMsg.response.data.message}`}</div>}
             {/* form */}
-                <form className='h-16 bg-black/30 w-full max-w-[450px] rounded-full backdrop-blur-[32px] mb-8'>
+                <form className={`${animate ? 'animate-shake': 'animate-none'} h-16 bg-black/30 w-full max-w-[450px] rounded-full backdrop-blur-[32px] mb-8`}>
                     <div className='h-full relative flex items-center justify-between p-2'>
-                        <input type="text" className='flex-1 bg-transparent outline-none placeholder:text-white text-white text-[15px] font-light pl-6 h-full' placeholder='search by city or country'/>
-                        <button className='bg-[#1ab8eb] hover:bg-[#15abdd] w-20 h-12 flex rounded-full felx justify-center items-center transition'>
+                        <input onChange={(e)=>handleInput(e)} type="text" className='flex-1 bg-transparent outline-none placeholder:text-white text-white text-[15px] font-light pl-6 h-full' placeholder='search by city or country'/>
+                        <button onClick={(e)=> handleSubmit(e)} className='bg-[#1ab8eb] hover:bg-[#15abdd] w-20 h-12 flex rounded-full felx justify-center items-center transition'>
                             <IoMdSearch className='text-2xl text-white'/>
                         </button>
                     </div>
                 </form>
             {/* card */}
                 <div className='w-full bg-black/20 max-w-[450px] min-h-[584px] text-white backdrop-blur-[32px] rounded-[32px] py-12 px-6'>
-                    <div>
+                    {loading ? (<div className='w-full h-full flex justify-center items-center'>
+                        <ImSpinner8 className='text-white text-5xl animate-spin'/>
+                    </div> 
+
+                    ) : (
+                        <div>
                       {/* card top */}
                         <div className='flex items-center gap-x-5'>
                             {/* icon */}
@@ -180,6 +249,8 @@ const Weather = () => {
                             </div>
                         </div>
                     </div>
+
+                    )}
                 </div>
             </div>
         </Fragment>
